aa5c9f361ed23e8319a4bc4efe516cbe44732032
Michael <michaelsaraivatoniolo@gmail.com>
2023-02-13 14:56:36 -0300

    Criação de Branch alfa para testes

--- projeto1/conectadb.php
+++ projeto1/conectadb.php
@@ -1,6 +1,11 @@
 <?php
+#LOCALIZA PC COM BANCO(NOME DO COMPUTADOR)
 $servidor = "localhost";
+#NOME DO BANCO
 $banco = "projeto1";
+#USUARIO DE ACESSO
 $usuario = "admin";
+#SENHA DO USUARIO
 $senha = "123";
-$link = mysqli_connect($servidor,$usuario,$senha,$banco);
+#LINK DE ACESSO
+$link = mysqli_connect($servidor, $usuario, $senha, $banco);
---
+++ projeto1/listausuario.php
@@ -0,0 +1,54 @@
+<?php
+#abre conexão com o banco de dados
+include("conectadb.php");
+
+#passa a instrução para o bando de dados
+#função da instrução: LISTAR TODOS O CONTEÚDO DA TABELA usuarios
+$sql = "SELECT * FROM usuarios";
+$resultado = mysqli_query($link, $sql);
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LISTA USUARIOS</title>
+    <link rel="stylesheet" href="estilo.css">
+
+</head>
+
+<body>
+    <a href="homesistema.html"><input type="button" id="menuhome" value="HOME SISTEMA"></a>
+    <div class="container">
+        
+        <table border="1">
+            <tr>
+                <th>NOME</th>
+                <th>ALTERAR DADOS</th>
+                <th>ATIVO</th>
+            </tr>
+            <?php
+                while ($tbl = mysqli_fetch_array($resultado)){
+                    ?>
+                    <tr>
+                        <td><?= $tbl[1]?></td> <!-- traz somente a coluna nome para apresentar na tabela-->
+                        <!-- Ao clicar no botão ele já trará o id do usuario para a página do alterar -->
+                        <td><a href="alterausuario.php?id=<?= $tbl[0]?>"><input type="button" value="ALTERAR"></a></td>
+                         <!-- Ao clicar no botão ele já trará o id do usuario para a página do excluir -->
+                        <!-- <td><a href="excluiusuario.php?id=<//?=$tbl[0]?>"><input type="button" value="EXCLUIR"></a></td> -->
+                        <td><?= $check = ($tbl[3] == "s")?"SIM":"NÃO"?></td>
+                        
+                    </tr>
+                    <?php
+                }
+            ?>
+        </table>
+    </div>
+</body>
+
+</html>
